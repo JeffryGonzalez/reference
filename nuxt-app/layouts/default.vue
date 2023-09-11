b81b5e65241ea226f7ddc72c570bb97b116c3cc1
Jeffry Gonzalez <jeff@hypertheory.com>
2023-09-11 18:16:52 -0400

    colormode

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="container mx-auto">
+    <header>
+      <ColorMode />
+    </header>
     <div />
     <slot />
   </section>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   ],
   devtools: { enabled: true },
   modules: [
-
+    '@nuxtjs/color-mode',
     '@nuxtjs/eslint-module'
   ],
   eslint: {
