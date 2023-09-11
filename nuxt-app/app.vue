c60d41d37bc79a560d2dc882353a6a374b56467d
Jeffry Gonzalez <jeff@hypertheory.com>
2023-09-10 21:25:26 -0400

    eslint

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <h1 class="text-blue-900 text-3xl font-bold">Welcome</h1>
+    <h1 class="text-blue-900 text-3xl font-bold">
+      Welcome
+    </h1>
   </div>
 </template>
